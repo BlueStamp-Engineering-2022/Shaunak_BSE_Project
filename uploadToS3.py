1f14799feb78ee5c1ca50a65296023e4201aebf0
Shaunak <61204264+drearydepp@users.noreply.github.com>
2022-06-29 09:28:05 -0700

    Update uploadToS3.py
    Updated with new bucket name and new jpeg images.

--- uploadToS3.py
+++ uploadToS3.py
@@ -1,13 +1,16 @@
 import boto3
 s3 = boto3.resource('s3')
 # Get list of objects for indexing
-images=[('shaunak.jpg','Shaunak Dam')]
+images=[('peoplepicture1.jpeg','Napoleon Bonaparte'),
+       ('peoplepicture2.jpeg','Jone Smith'),
+       ('peoplepicture3.jpeg','John Smith')]
 # Iterate through list to upload objects to S3   
 for image in images:
    file = open(image[0],'rb')
-   object = s3.Object('guest.images',image[0])
+   object = s3.Object('shaunak2',image[0])
    ret = object.put(Body=file,
                    Metadata={'FullName':image[1]}
                    )
    print(image[0])
    print(image[1])
+
